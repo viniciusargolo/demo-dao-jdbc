88cdc90b0fff8361b2e27a82c4b5976d365d19db
viniciusargolo <viniciusargolo.0@gmail.com>
2026-04-22 03:02:41 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,20 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDAO;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+	
+		SellerDAO sellerDao = DaoFactory.CreateSellerDao();
 		
-		Seller seller = new Seller(21, "Bob", "bob@email.com", new Date(), 3000.0, obj);
+		Seller seller = sellerDao.findById(3);
+		
 		System.out.println(seller);
 		
-		SellerDAO sellerDao = DaoFactory.CreateSellerDao();
 	}
 
 }
